8c7bbf975f7494b31352842c10d90dc1aaa1f515
sebastian <sebastian.nadirgmail.com>
2020-05-04 14:34:43 -0400

    final push

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,20 @@
+
 #!/usr/bin/env bash
 
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=sebastianreyes/snreyess-app:v0.1.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment app --image=snreyess-app:v0.1.0
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment app --type=LoadBalancer --port=8000 --target-port=80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=sebastianreyes/snreyess-app:v0.1.0
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username sebastianreyes
+docker tag snreyess-app:v0.1.0 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
